2472f80f106f86170dc5f0f40e2ea11778fdbaa1
fjquen <fjquen@gmail.com>
2024-05-12 18:42:54 +0200

    position random fonctionel

--- main.rb
+++ main.rb
@@ -17,29 +17,44 @@ class Main < Gosu::Window
                 ["#"," "," "," "," "," "," "," "," "," "," ","#"],
                 ["#","#","#","#","#","#","#","#","#","#","#","#"]]
         @player = Player.new
-        tabMap = @world.map { |row| row.each_index.select { |i| row[i] != "#" } }
-        x_position = tabMap[rand(tabMap.size)].size*40 
-        y_position = rand(0..tabMap[rand(tabMap.size)].size)*40
-        @player.x, @player.y = x_position == 0 ? 40 : x_position,y_position == 0 ? 40 : y_position
+        @tabMap = @world.map { |row| row.each_index.select { |i| row[i] != "#" } }
+        x_position = @tabMap[rand(@tabMap.size)].size*42 
+        y_position = rand(0..@tabMap[rand(@tabMap.size)].size)*42
+        @player.x, @player.y = x_position == 0 ? 42 : x_position,y_position == 0 ? 42 : y_position
+        @width=@higth = 40
     end
 
     def update
-        if Gosu.button_down? Gosu::KB_RIGHT
-			puts "d"
-        end
-        if Gosu.button_down? Gosu::KB_LEFT
-			puts "g"
-        end
+        if @world[@player.y/40][@player.x/40]!="#"
+		    @player.y+=1
+	    end
+		
+		if Gosu.button_down? Gosu::KB_RIGHT
+			@player.x+=1
+		end
+		
+		if Gosu.button_down? Gosu::KB_LEFT
+			@player.x-=1
+		end	
     end
     
-    
 
+
+	def button_down(id)
+		case id
+			when Gosu::KB_UP
+			  @player.y-=18
+			when Gosu::KB_ESCAPE
+			  close
+		end
+    end
+    
     def draw
         @player.draw
         @world.each_index do |row|
             @world[row].each_index do |col|
                 if @world[row][col] == "#"
-                    Gosu.draw_rect(col*40, row*40, 40, 40,Gosu::Color::BLUE)
+                    Gosu.draw_rect(col*40, row*40, @width, @higth,Gosu::Color::BLUE)
                 end
             end
         end
--- player.rb
+++ player.rb
@@ -1,12 +1,12 @@
 class Player
-    attr_accessor :x,:y
+    attr_accessor :x,:y,:l,:h
     def initialize
         @x=@y=0
+        @l=@h=20
     end
     
     
-    
     def draw
-        Gosu.draw_rect(@x, @y, 15, 20,Gosu::Color::RED)
+        Gosu.draw_rect(@x, @y, @l, @h,Gosu::Color::RED)
     end
 end
